6ecda8a0732b02fffd8e9876d66efdef9407cf5b
Datu Arellano <datuarellano@gmail.com>
2023-08-26 14:48:55 +0800

    refactor mic and piezo inputs

--- include/inputs.h
+++ include/inputs.h
@@ -2,25 +2,26 @@
 //// INPUTS : Mic and piezo ///////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
-void inputs() {
+void inputs()
+{
   // TODO inputs
   // Electret Mic
   if (sw_3_1.read() == LOW && sw_3_1_state == false)
   {
     sw_3_1_state = true;
-    mixer2.gain(2, 0.9);                   // piezo
-    mixer2.gain(3, (param1_val * ANALOG)); // mic
-    }
-    else if (sw_3_1.read() == HIGH && sw_3_1_state == true)
-    {
-      sw_3_1_state = false;
-      mixer2.gain(2, 0); // piezo
-      mixer2.gain(3, 0); // mic
-    }
-    else 
-    {
-      sw_3_1_state = false;
-      mixer2.gain(2, 0); // piezo
-      mixer2.gain(3, 0); // mic
-    }
+    mixer2.gain(2, (param1_val * ANALOG)); // mic
+    mixer2.gain(3, 0.9);                   // piezo
+  }
+  else if (sw_3_1.read() == HIGH && sw_3_1_state == true)
+  {
+    sw_3_1_state = false;
+    mixer2.gain(2, 0); // mic
+    mixer2.gain(3, 0); // piezo
+  }
+  else
+  {
+    sw_3_1_state = false;
+    mixer2.gain(2, 0); // mic
+    mixer2.gain(3, 0); // piezo
+  }
 }
