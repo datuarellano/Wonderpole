e1d6096fd548d30b1291cc997b4ad3c0566ebde3
Datu Arellano <datuarellano@gmail.com>
2023-08-30 23:44:08 +0800

    refactor inputs, removing distortion from mic in

--- include/inputs.h
+++ include/inputs.h
@@ -5,23 +5,20 @@
 void inputs()
 {
   // TODO inputs
-  // Electret Mic
-  if (sw_3_1.read() == LOW && sw_3_1_state == false)
+  float gain = param1_val * ANALOG;
+
+  if (sw_3_1.read() == HIGH && sw_3_2.read() == HIGH)
   {
-    sw_3_1_state = true;
-    mixer2.gain(2, (param1_val * ANALOG)); // mic
-    mixer2.gain(3, 0.9);                   // piezo
+    mixer2.gain(2, 0);
+    mixer2.gain(3, 0);
   }
-  else if (sw_3_1.read() == HIGH && sw_3_1_state == true)
+  else if (sw_3_2.read() == LOW)
   {
-    sw_3_1_state = false;
-    mixer2.gain(2, 0); // mic
-    mixer2.gain(3, 0); // piezo
+    mixer2.gain(2, 0);
+    mixer2.gain(3, gain);
   }
-  else
-  {
-    sw_3_1_state = false;
-    mixer2.gain(2, 0); // mic
-    mixer2.gain(3, 0); // piezo
+  else {
+    mixer2.gain(2, gain);
+    mixer2.gain(3, gain);
   }
 }
