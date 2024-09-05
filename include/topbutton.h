37cb6734738863c424831c1d5fd8dd93bd81ff6e
Datu Arellano <datuarellano@gmail.com>
2024-09-05 21:21:10 +0800

    modify Top Button sound design

--- include/topbutton.h
+++ include/topbutton.h
@@ -4,17 +4,18 @@
 
 void topbutton() {
   // TODO Top button trigger
-  drum_amp.gain(5);
-  drum.length(100);
+  drum_amp.gain(10);
+  drum.length(random(500,900));
   drum.secondMix(0.7);
   drum_pinknoise.amplitude(0.7);
   // drum_filter.frequency(400);
   // drum_filter.resonance(5.0);
   // drum_bitcrusher.bits(7);
+  drum_noise_tremolo.amplitude(0.6);
 
   if (btn_top.read() == LOW && btn_top_state == false)
   {
-    drum.frequency(random(40,50));
+    drum.frequency(random(20,30));
     // drum.frequency(map(rib_val, 0, 1023, 20, 80));
     btn_top_state = true;
     drum_noise_dc.amplitude(0.05,5);
@@ -24,6 +25,11 @@ void topbutton() {
   else if (btn_top.read() == HIGH && btn_top_state == true)
   {
     btn_top_state = false;
-    drum_noise_dc.amplitude(0,20);
+    drum_noise_dc.amplitude(0,300);
+  }
+
+  if (noteSequenceTimer.hasPassed(500)) {
+    drum_noise_tremolo.frequency(random(1, 8));
+    noteSequenceTimer.restart();
   }
 }
